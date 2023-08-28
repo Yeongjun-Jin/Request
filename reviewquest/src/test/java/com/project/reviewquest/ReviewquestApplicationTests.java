f60d91ff264e696c1a304adf989361b3ef148a24
UserK <UserK@DESKTOP-EMC2Q9M>
2023-08-28 16:25:56 +0900

    test3

---
+++ reviewquest/src/test/java/com/project/reviewquest/ReviewquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.reviewquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
